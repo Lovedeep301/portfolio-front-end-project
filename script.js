696ec5b0f152eed7f928847103c4a27b7487ba19
Lovedeep301 <139338222+Lovedeep301@users.noreply.github.com>
2024-11-21 18:24:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
